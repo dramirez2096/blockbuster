1efb70ba9a6d9349ac07c1d584e5f7de90f76a46
David Ramirez <dramirez2096@gmail.com>
2018-04-21 23:51:38 -0700

    assets for ball sprite created

--- js/game.js
+++ js/game.js
@@ -1,9 +1,17 @@
 var game;
 
+var globalOptions = {
+    ballSize: 0.04
+}
+
 window.onload = function() {
     game = new Phaser.Game(500, 400, Phaser.CANVAS);
-
     game.state.add("PlayGame", playGame, true);
 }
 
 var playGame = function(){}
+playGame.prototype = {
+    preload: function(){
+        game.load.image("ball", "img/ball-sprite.png");
+    }
+}
