31de7a49b4d65acc01cc1e37366c0ba9ee63b6c5
David Ramirez <dramirez2096@gmail.com>
2018-04-22 00:01:31 -0700

    code for ball sprite added, ball still doesn't appear

--- js/game.js
+++ js/game.js
@@ -13,5 +13,12 @@ var playGame = function(){}
 playGame.prototype = {
     preload: function(){
         game.load.image("ball", "img/ball-sprite.png");
+    },
+    create: function(){
+        var ballSize = game.width * globalOptions.ballSize;
+        this.ball = game.add.sprite(game.width / 2, "ball");
+        this.ball.width = ballSize;
+        this.ball.height = ballSize;
+        this.ball.anchor.set(0.5);
     }
 }
