5c58b93e9013a7f648abaab0a3c9b70864d03fe1
David Ramirez <dramirez2096@gmail.com>
2018-04-22 22:12:43 -0700

    panels added

--- js/game.js
+++ js/game.js
@@ -1,7 +1,9 @@
 var game;
 
 var globalOptions = {
-    ballSize: 0.04
+    ballSize: 0.04,
+    scorePanelHeight: 0.08,
+    bottomPanelHeight: 0.18
 }
 
 window.onload = function() {
@@ -13,6 +15,7 @@ var playGame = function(){}
 playGame.prototype = {
     preload: function(){
         game.load.image("ball", "img/ball-sprite.png");
+        game.load.image("panel", "img/panel.png");
     },
     create: function(){
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -20,8 +23,17 @@ playGame.prototype = {
         game.scale.pageAlignVertically = true;
         game.stage.backgroundColor = 0x202020;
 
+        var scorePanel = game.add.image(0, 0, "panel");
+        scorePanel.width = game.width;
+        scorePanel.height = Math.round(game.height * globalOptions.scorePanelHeight);
+
+        this.bottomPanel = game.add.sprite(0, game.height, "panel");
+        this.bottomPanel.width = game.width;
+        this.bottomPanel.height = Math.round(game.height * globalOptions.bottomPanelHeight);
+        this.bottomPanel.anchor.set(0, 1);
+
         var ballSize = game.width * globalOptions.ballSize;
-        this.ball = game.add.sprite(game.width / 2, game.height - ballSize / 2, "ball");
+        this.ball = game.add.sprite(game.width / 2, game.height - this.bottomPanel.height - ballSize / 2, "ball");
         this.ball.width = ballSize;
         this.ball.height = ballSize;
         this.ball.anchor.set(0.5);
